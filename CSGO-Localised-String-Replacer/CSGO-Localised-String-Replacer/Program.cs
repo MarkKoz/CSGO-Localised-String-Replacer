66bc53a8c1a3699b3e6e0dc106b9a67e4195d448
MarkKoz <KozlovMark@gmail.com>
2016-05-20 01:13:20 -0700

    Updated Readme and Project Properties

---
+++ CSGO-Localised-String-Replacer/CSGO-Localised-String-Replacer/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Text;
+using System.IO;
+
+namespace CSGO_Localised_String_Replacer
+{
+    public class LocalisedStringReplacer
+    {
+        public static void ReplaceStrings(string pathCsgo, string pathIni)
+        {
+            if (File.Exists(pathCsgo))
+            {
+                Console.Write(Path.GetFileName(pathCsgo) + " was found." + "\n");
+                if (File.Exists(pathIni))
+                {
+                    Console.Write(Path.GetFileName(pathIni) + " was found." + "\n\n");
+
+                    string[] contents = File.ReadAllLines(pathCsgo);
+                    Console.Write("Parsing " + Path.GetFileName(pathCsgo) + "\n");
+
+                    string[] strings = File.ReadAllLines(pathIni);
+                    Console.Write("Parsing " + Path.GetFileName(pathIni) + "\n\n");
+
+                    for (int i = 0; i < contents.Length; i++)
+                    {
+                        var line = contents[i];
+
+                        foreach (var str in strings)
+                        {
+                            var equalsIndex = str.IndexOf('=');
+                            if (equalsIndex != -1)
+                            {
+                                var field = "\"" + str.Substring(0, equalsIndex) + "\"";
+
+                                if (line.Contains(field))
+                                {
+                                    Console.Write("Found string " + field + " at line " + i + "\n");
+                                    var translation = str.Substring(equalsIndex + 1, str.Length - equalsIndex - 1);
+                                    contents[i] = field + "\t" + translation;
+                                    Console.Write("Replaced value of string " + field + " at line " + i + " with " + translation + "\n\n");
+                                }
+                            }
+                        }
+                    }
+                    Console.Write("Writing changes to " + Path.GetFileName(pathCsgo) + "\n\n");
+                    File.WriteAllLines(pathCsgo, contents, Encoding.Unicode);
+
+                    Console.Write("Finished!" + "\n");
+                }
+                else
+                {
+                    Console.Write(Path.GetFileName(pathIni) + " was not found at the given directory." + "\n");
+                }
+            }
+            else
+            {
+                Console.Write(Path.GetFileName(pathCsgo) + " was not found at the given directory." + "\n");
+            }
+        }
+        public static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.Write("The location of the CS:GO localisation file needs to be specified as the first command-line argument." + "\n");
+            }
+            else
+            {
+                if (args.Length == 1)
+                {
+                    Console.Write(
+                        "The location of the replacer configuration file needs to be specified as the second command-line argument." + "\n");
+                }
+                else
+                {
+                    ReplaceStrings(args[0], args[1]);
+                }
+            }
+            Console.Read();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # CSGO-Localised-String-Replacer
-A simple tool that replaces CSGO's localised strings with strings from a user-created file.
+
+## Description
+A simple tool that replaces CSGO's localised strings with strings from a user-created file. Useful for customising colours of chat notifications (e.g. money awarded) for languages other than English.
+
+## Requirements
+- Windows operating system
+- .NET Framework 4.5.2 or later
+- CSGO (probably works with other Source games or any similarily-structured file)
+
+## Instructions
+1. Create an INI file and specify the names of the strings to replace and their values in the following format:
+  ```
+  Name="Value"
+  ```
+  For example:
+  ```
+  SFUI_Lobby_StatusRichPresence_competitive_lobby="MM lobby"
+  SFUI_Lobby_StatusRichPresence_competitive_game="MM -"
+  ```
+2. Open a command prompt at the location of `CSGO-Localised-String-Replacer.exe` and run the program, specifying the location of the locale file and the INI file. For example:
+  ```
+  C:\> CSGO-Localised-String-Replacer.exe "C:\Program Files (x86)\Steam\steamapps\common\Counter-Strike Global Offensive\csgo\csgo_english.txt" "C:\strings.ini"
+  ```
+
+## Credits
+
+Mark Kozlov
+
+Nima Shoghi
+
